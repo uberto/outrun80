3062e092b87047572c0e7946be7d299c4bbddabc
Uberto Barbini <uberto.gama@gmail.com>
2020-06-14 21:35:04 +0100

    added hills

--- outrun.lua
+++ outrun.lua
@@ -13,26 +13,25 @@ halfH=resH/2
 
 
 roadW=2000 --road width
-segL=200   --track segment lenght
+segL=400   --track segment lenght
 camD=0.84  --camera depth
 
 
-posH=0       --position Horizontal (left-right)
-dH=20        --delta to move horizontally with a key press
-posV=1500    --position Vertical (high-low)
-spd=0        --speed
-prog=0       --current progress on the track 
-lookAhead=6  --how many seg ahead start drawing
+posH=0          --position Horizontal (left-right)
+posV=1500       --position Vertical (high-low)
+spd=0           --speed
+prog=0          --current progress on the track 
+trackX=0        --horizontal position of the track
+trackLen = 2000 --length of the track
+dH = 60         --left right delta by keys
 
-
-trackLen = 10000 --length of the track
 
 function keys()
 
-	if btn(0) then spd=spd+1 end
+	if btn(0) and spd < 180 then spd=spd+1 end
 	if btn(1) and spd > 0 then spd=spd-1 end
-	if btn(2) then posH=posH-dH end
-	if btn(3) then posH=posH+dH end
+	if btn(2) and spd > 0 then posH=posH + dH end
+	if btn(3) and spd > 0 then posH=posH - dH end
 end
 
 
@@ -44,21 +43,30 @@ function drawQuad( x1, y1, w1, x2, y2, w2, color)
 	
 end
 
-function trackSeg(ax,ay,az,aw)
+function trackSeg(ax,ay,az,aw,c)
 
  return 	{x	= ax,	
          	y = ay,
          	z = az,
-          w = aw}	
+          w = aw,
+										curve = c}	
 end
 
 function project(seg, camX, camY, camZ)
 
+  if seg.z == camZ then 
+	   scale = camD	
+  elseif seg.z <= camZ then 
+		  trace("!!! segz:"..seg.z.." camZ"..camZ)
+
+	   scale = camD / (seg.z + maxProg - camZ)	
+		else 
+	   scale = camD / (seg.z - camZ)	
+		end
 --	trace("project x"..seg.x.." y"..seg.y.." z"..seg.z)
 --	trace("project camX"..camX.." camY"..camY.." camZ"..camZ)
 
- scale = camD / (seg.z - camZ)	
-	
+ 
 	X = (1 + scale*(seg.x-camX)) * halfW		
 	Y = (1 - scale*(seg.y-camY)) * halfH
 	W = scale * seg.w * halfW
@@ -67,10 +75,40 @@ function project(seg, camX, camY, camZ)
 	return X, Y, W
 end
 
+function calcHeight(seg)
+
+  if seg > 300 and seg < 700 then 
+		  return math.sin( (seg-300) / 30) * 1250 
+		end
+		
+		if seg > 1500 and seg < 1800 then
+		  return math.sin( (seg-1500) / 30) * 125	0 
+		end
+		
+		return 0
+end		
+	
+function calcCurve(seg)
+
+		if seg > 100 and seg < 200 then return 0.5 end 
+		if seg > 100 and seg < 300 then return 1.0 end 
+
+		if seg > 700 and seg < 800 then return -1.0 end 
+		if seg > 800 and seg < 900 then return -1.5 end 
+
+		if seg > 1000 and seg < 1100 then return -0.5 end 
+		if seg > 1100 and seg < 1200 then return -1.5 end 
+		if seg > 1300 and seg < 1400 then return 2.0 end
+		
+		return 0
+end
+
+
 function createTrack()
   t = {}
   for i=0, trackLen do
-    t[i] = trackSeg(0,0,1+i*segL,roadW)
+		
+    t[i] = trackSeg(0,calcHeight(i),1+i*segL,roadW,calcCurve(i))
   end
 		
 		trace("prepared track " .. trackLen)
@@ -82,6 +120,8 @@ end
 
 track = createTrack()
 
+maxProg = trackLen * segL
+
 function TIC()
 --main function
 	cls(13)
@@ -90,37 +130,53 @@ function TIC()
 	
 	prog = prog + spd
 	
-	curr = prog // segL + lookAhead
+	curr = prog // segL	
 
- c = curr % trackLen
---	trace("curr:" .. curr .." c:".. c.." prog:"..prog)
- 
-	pX,pY,pW = project(track[c], posH, posV, prog)
+ cp = curr % trackLen
+ seg = track[cp]
+	pX,pY,pW = project(seg, posH, posV, prog)
 	
- for n = curr+1, curr+50 do
+	posH = posH - seg.curve * spd * 0.2
+	dx = 0
+	xx = 0
+ minY = resH
+ for n = curr+1, curr+100 do
 	  
 			c = n % trackLen
-			X,Y,W = project(track[c], posH, posV, prog)
+			seg = track[c]
+		
+		 xx = xx + dx
+			dx = dx + seg.curve
+			--trace("x:"..x.." dx"..dx)
 			
-			alt = (n // 4) % 2 == 0
-			
+			X,Y,W = project(seg, xx - posH, posV, prog)
+
+   if minY > Y then 
+--trace("curr:" .. curr .." c:".. c.." prog:"..progM.." Y"..Y)
 				
-		 if alt then grass=5 else grass=11 end
-		
-			if alt then rumble=0 else rumble=15 end
-		
-		 if alt then road=3 else road=7 end
+				alt = (n // 4) % 2 == 0
+				
+						
+			 if alt then grass=5 else grass=11 end
+			
+				if alt then rumble=0 else rumble=15 end
+			
+			 if alt then road=3 else road=7 end
+	
+	  
+	   drawQuad(0,  pY, resW,    0, Y, resW, grass)	
+	   drawQuad(pX, pY, pW *1.1, X , Y, W *1.1, rumble)	
+	   drawQuad(pX, pY,	pW,      X	, Y, W, road)	
 
-  
-   drawQuad(0,  pY, resW,    0, Y, resW, grass)	
-   drawQuad(pX, pY, pW + 10, X , Y, W + 10, rumble)	
-   drawQuad(pX, pY,	pW,      X	, Y, W, road)	
-
+    minY = Y
+   end
 			pX = X
 			pY = Y
 			pW = W
 			
 	end
+	
+	print("s:"..spd.."  p:"..cp)
 
 end
 
